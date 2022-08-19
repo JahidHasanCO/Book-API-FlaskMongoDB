b56dc659e86f4105648c3330377d6c7e5b9bfcb5
Jahid hasan <Vdjsovaj@gmail.com>
2022-08-19 19:29:57 +0600

    update schema

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, make_response, request, render_template
 from flask_mongoengine import MongoEngine
 from api_constants import mongodb_password
 
+
 app = Flask(__name__, template_folder='templates')
 
 database_name = "API"
@@ -16,14 +17,40 @@ db.init_app(app)
 
 class Book(db.Document):
     book_id = db.IntField()
-    name = db.StringField()
+    title = db.StringField()
     author = db.StringField()
+    publisher = db.StringField()
+    publishedDate = db.StringField()
+    description = db.StringField()
+    pageCount = db.StringField()
+    printedPageCount = db.StringField()
+    language = db.StringField()
+    printType = db.StringField()
+    averageRating = db.DoubleField()
+    maturityRating = db.StringField()
+    ratingsCount = db.IntField()
+    imageLinks = []
 
     def to_json(self):
+
+        VolumnInfo = {
+            "title": self.title,
+            "author": self.author,
+            "publisher": self.publisher,
+            "publishedDate": self.publishedDate,
+            "description": self.description,
+            "pageCount": self.pageCount,
+            "printedPageCount": self.printedPageCount,
+        }
         return {
             "book_id": self.book_id,
-            "name": self.name,
-            "author": self.author
+            "VolumnInfo": VolumnInfo,
+            "imageLinks": self.imageLinks,
+            "language": self.language,
+            "printType": self.printType,
+            "averageRating": self.averageRating,
+            "ratingsCount": self.ratingsCount,
+            "maturityRating": self.maturityRating
         }
 
 
@@ -33,12 +60,39 @@ def index():
     return render_template("index.html")
 
 
+# Book(book_id =1,
+#     title = "",
+#     author =,
+#      publisher=,
+#      publishedDate=,
+#      description=,
+#      pageCount=,
+#      printedPageCount=,
+#      language=,
+#      printType=,
+#      averageRating=,
+#      maturityRating=,
+#      ratingsCount=,
+#      imageLinks=)
+
 @app.route('/api/db_populate/', methods=['POST'])
 def db_populate():
-    book1 = Book(book_id=1, name="Game of Thrones", author="Geargo RR Martin")
-    book2 = Book(book_id=2, name="Lord of the Rings", author="JRR Tolkien")
+    images = []
+    book1 = Book(book_id=1,
+                 title="Pride and Prejudice",
+                 author="Jane Austen",
+                 publisher="C. Scribner's sons",
+                 publishedDate="1918",
+                 description="Austen’s most celebrated novel tells the story of Elizabeth Bennet, a bright, lively young woman with four sisters, and a mother determined to marry them to wealthy men. At a party near the Bennets’ home in the English countryside, Elizabeth meets the wealthy, proud Fitzwilliam Darcy. Elizabeth initially finds Darcy haughty and intolerable, but circumstances continue to unite the pair. Mr. Darcy finds himself captivated by Elizabeth’s wit and candor, while her reservations about his character slowly vanish. The story is as much a social critique as it is a love story, and the prose crackles with Austen’s wry wit.",
+                 pageCount=401,
+                 printedPageCount=448,
+                 language="en",
+                 printType="BOOK",
+                 averageRating=4,
+                 maturityRating="NOT_MATURE",
+                 ratingsCount=400,
+                 imageLinks=images)
     book1.save()
-    book2.save()
     return make_response("", 201)
 
 
@@ -51,8 +105,20 @@ def db_books():
         return make_response(jsonify(books), 200)
     elif request.method == "POST":
         content = request.json
-        book = Book(book_id=content['book_id'], name=content['name'],
-                    author=content['author'])
+        book = Book(book_id=content['book_id'],
+                    title=content['title'],
+                    author=content['author'],
+                    publisher=content['publisher'],
+                    publishedDate=content['publishedDate'],
+                    description=content['description'],
+                    pageCount=content['pageCount'],
+                    printedPageCount=content['printedPageCount'],
+                    language=content['language'],
+                    printType=content['printType'],
+                    averageRating=content['averageRating'],
+                    maturityRating=content['maturityRating'],
+                    ratingsCount=content['ratingsCount'],
+                    imageLinks=content['imageLinks'])
         book.save()
         return make_response("", 201)
 
@@ -68,7 +134,20 @@ def db_each_book(book_id):
     elif request.method == "PUT":
         content = request.json
         book_obj = Book(book_id=book_id).first()
-        book_obj.update(author=content['author'], name=content['name'])
+        book_obj.update(book_id=content['book_id'],
+                        title=content['title'],
+                        author=content['author'],
+                        publisher=content['publisher'],
+                        publishedDate=content['publishedDate'],
+                        description=content['description'],
+                        pageCount=content['pageCount'],
+                        printedPageCount=content['printedPageCount'],
+                        language=content['language'],
+                        printType=content['printType'],
+                        averageRating=content['averageRating'],
+                        maturityRating=content['maturityRating'],
+                        ratingsCount=content['ratingsCount'],
+                        imageLinks=content['imageLinks'])
         return make_response("", 204)
     elif request.method == "DELETE":
         book_obj = Book(book_id=book_id).first()
